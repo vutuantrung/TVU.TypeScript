24297e051df3399992251de8345b42d248181d27
VUTuanTrung <trung.lespace@gmail.com>
2021-02-09 16:38:02 +0100

    advanced types

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -1,1 +1,140 @@
-// Code goes here!
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Trung',
+  privileges: ['create-server'],
+  startDate: new Date()
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+
+// Type guards
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  // These property doesn't work: privileges, startDate
+  //   console.log('Privileges: ' + privileges);
+
+  if ('privileges' in emp) {
+    console.log('Privileges' + emp.privileges);
+  }
+
+  if ('startDate' in emp) {
+    console.log('StartDate' + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+const v1 = new Car();
+const v2 = new Truck();
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+  console.log('Moving at speed: ' + speed);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
+
+// Type casting
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+const userInputElement = document.getElementById('user-input');
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there!';
+}
+
+interface ErrorContainer {
+  id: string;
+  // { email: 'Not a valid email', userName: 'Must start with a character' }
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  id: '2',
+  email: 'Not a valid email!',
+  userName: 'Must start with a capital character!'
+};
+
+// Function overload
+function combineVal(a: number, b: number): number;
+function combineVal(a: string, b: string): string;
+function combineVal(a: number, b: string): string;
+function combineVal(a: string, b: number): string;
+function combineVal(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const resultCombineVal = combineVal('Trung', 'Hello');
+
+// Optional chaining
